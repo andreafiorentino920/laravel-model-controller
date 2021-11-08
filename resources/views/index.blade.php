c7c04d4a3f7d90e400d95859d2d09dd82072cf52
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-11-08 17:55:22 +0100

    Esercizio completo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,24 @@
-@foreach ($movies as $movie)
-<p>{{$movie["title"]}}</p>
-    
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie Prova</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div class="container">
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <h1>Titolo: {{$movie['title']}}</h1>
+                    <h2>Titolo Originale: {{$movie['original_title']}}</h2>
+                    <h4>Nazionalità: {{$movie['nationality']}}</>
+                    <h4>Data di uscita: {{$movie['date']}}</h4>
+                    <h4>Voto della critica: {{$movie['vote']}}</h4>
+                </div>
+            @endforeach
+        </ul>
+    </div>
+</body>
+</html>
